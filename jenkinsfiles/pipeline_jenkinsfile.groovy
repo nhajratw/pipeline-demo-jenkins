f3f12f0ac73eed5ded0a4c567e00937bff901788
Nayan Hajratwala <nayan@chikli.com>
2017-05-03 10:15:30 -0400

    add deployment

--- jenkinsfiles/pipeline_jenkinsfile.groovy
+++ jenkinsfiles/pipeline_jenkinsfile.groovy
@@ -51,52 +51,12 @@ pipeline {
         sh "git push origin --tags"
       }
     }
-  }
-/*
 
-
-  stage('retrieve artifacts') {
-    steps {
-      def remoteLocation =
-      "http://www.nexus.ford.com/content/repositories/goe_private_release_repository/com/ford/gotd/goe-services/${releaseVersion}"
-      sh "curl -u gotdgoe:FED[3y ${remoteLocation}/goe-services-${releaseVersion}.jar -o /tmp/goe-services-${releaseVersion}.jar"
-      sh "curl -u gotdgoe:FED[3y ${remoteLocation}/goe-services-${releaseVersion}-manifests.zip -o
-      /tmp/goe-services-${releaseVersion}-manifests.zip"
+    stage('deploy') {
+      steps {
+        sh "cf login -a api.local.pcfdev.io --skip-ssl-validation -u admin -p admin -o demo -s pipeline"
+        sh "cf push pipeline-demo-service -p build/libs/pipeline-demo-service-${releaseVersion}.jar"
+      }
     }
   }
-
-  stage('deploy') {
-    steps {
-      sh "rm -rf ~/.cf/ecc ~/.cf/fmc"
-      def username = "goe-ci"
-      def password = "Goe2016!"
-      parallel (
-        "ecc services demo" : { deploy("ecc", "goe-services", "demo", "jar", username, password, "Beta") },
-        "fmc services demo" : { deploy("fmc", "goe-services", "demo", "jar", username, password, "Beta") },
-        "ecc services ci"   : { deploy("ecc", "goe-services", "ci", "jar", username, password, "Alpha") },
-        "fmc services ci"   : { deploy("fmc", "goe-services", "ci", "jar", username, password, "Alpha") },
-      )
-    }
-  }
-*/
 }
-
-/*
-def deploy(dataCenter, artifactName, environment, type, username, password, space) {
-  def artifactLocation = "/tmp"
-  def expandedManifestLocation = "/tmp/${dataCenter}/${artifactName}"
-  sh "mkdir -p ${expandedManifestLocation} && unzip -o ${artifactLocation}/${artifactName}-${releaseVersion}-manifests.zip -d ${expandedManifestLocation}"
-  def manifestFile = "${expandedManifestLocation}/manifest-${environment}.yml"
-  def api = "https://api.sys-q01.pcfqa${dataCenter}.ford.com"
-  def cf_home = "~/.cf/${dataCenter}/${space}"
-  def domain = "apps-q01.pcfqa${dataCenter}.ford.com"
-
-  def command = 'mkdir -p ' + cf_home
-  command += "; export CF_HOME=${cf_home}"
-  command += "; cf api ${api} --skip-ssl-validation"
-  command += "; cf login -u ${username} -p ${password} -o Ford_GOE_NA -s ${space}"
-  command += "; cf push -f ${manifestFile} -p ${artifactLocation}/${artifactName}-${releaseVersion}.${type}"
-
-  retry(3) { sh command }
-}
-*/
