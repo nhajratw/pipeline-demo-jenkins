ef71e29f02895641c2ba418d2e25fdfbf372b599
Nayan Hajratwala <nayan@chikli.com>
2017-11-07 11:19:15 -0500

    remove errant comment

--- jenkinsfiles/pipeline_jenkinsfile.groovy
+++ jenkinsfiles/pipeline_jenkinsfile.groovy
@@ -38,7 +38,6 @@ pipeline {
       steps {
         sh """
           cf login -a api.local.pcfdev.io --skip-ssl-validation -u admin -p admin -o demo -s pipeline
-          //cf unmap-route pipeline-demo-service-a local.pcfdev.io --hostname pipeline-demo-service
 
           cf push pipeline-demo-service-a -p $WORKSPACE/build/libs/pipeline-demo-service-${releaseVersion}.jar
           cf map-route pipeline-demo-service-a local.pcfdev.io --hostname pipeline-demo-service
