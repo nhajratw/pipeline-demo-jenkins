e360d78680fd7feb491a04d5bb0b40084abf7b37
Nayan Hajratwala <nayan@chikli.com>
2017-11-07 10:59:51 -0500

    add git pull stage

--- jenkinsfiles/pipeline_jenkinsfile.groovy
+++ jenkinsfiles/pipeline_jenkinsfile.groovy
@@ -2,5 +2,10 @@ pipeline {
   agent any
 
   stages {
+    stage('pull') {
+      steps {
+        git url:'/Users/nayan/Documents/workspace/pipeline-demo/pipeline-demo-service', branch:'agileto2017'
+      }
+    }
   }
 }
